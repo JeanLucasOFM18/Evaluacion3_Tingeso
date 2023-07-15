5405c516e263de6e9ca7c032b3b5627080c7993d
Jean Lucas Rivera <jean.rivera@usach.cl>
2023-07-15 01:19:49 -0400

    Se realiza modificaciones en el diseño de la página

--- backend-pyquiz/repositorio-service/repositorio-service/src/main/resources/import.sql
+++ backend-pyquiz/repositorio-service/repositorio-service/src/main/resources/import.sql
@@ -1,5 +1,32 @@
-INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Suma v1', 'https://i.postimg.cc/3x0DZGGv/imagen-2023-07-13-150430631.png', '1', 'Basico');
-INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Suma v2', 'https://i.postimg.cc/wBKR4B5b/imagen-2023-07-13-150439396.png', '2', 'Basico');
-INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Suma v3', 'https://i.postimg.cc/26Gq5FsP/imagen-2023-07-13-150453776.png', '3', 'Basico');
-INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Suma v4', 'https://i.postimg.cc/J7TDVxcX/imagen-2023-07-13-150501089.png', '4', 'Basico');
-INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Suma v5', 'https://i.postimg.cc/XNcMp1DZ/imagen-2023-07-13-151144721.png', '5', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta1', 'https://i.postimg.cc/9MkGM2qN/Pregunta1-Basico.png', '15', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta2', 'https://i.postimg.cc/T1Wn5sV2/Pregunta2-Basico.png', '30', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta3', 'https://i.postimg.cc/RZTp03Cs/Pregunta3-Basico.png', '168', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta4', 'https://i.postimg.cc/QdDnsgDK/Pregunta4-Basico.png', '10', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta5', 'https://i.postimg.cc/T2Z4NMH9/Pregunta5-Basico.png', '16', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta6', 'https://i.postimg.cc/sfBLJDKx/Pregunta6-Basico.png', 'impar', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta7', 'https://i.postimg.cc/dtrSzHbK/Pregunta7-Basico.png', '5', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta8', 'https://i.postimg.cc/8CPKf1j1/Pregunta8-Basico.png', '120', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta9', 'https://i.postimg.cc/VNG7j3CX/Pregunta9-Basico.png', '2', 'Basico');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta10', 'https://i.postimg.cc/xCX6gtvh/Pregunta10-Basico.png', '12', 'Basico');
+
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta1', 'https://i.postimg.cc/fbmZHLkZ/Pregunta1-Intermedio.png', '4', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta2', 'https://i.postimg.cc/6pDtLRWY/Pregunta2-Intermedio.png', '1', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta3', 'https://i.postimg.cc/HW2WDVzk/Pregunta3-Intermedio.png', 'manzana', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta4', 'https://i.postimg.cc/L8qm1vpJ/Pregunta4-Intermedio.png', '15', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta5', 'https://i.postimg.cc/CLxhFSrH/Pregunta5-Intermedio.png', 'no', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta6', 'https://i.postimg.cc/bJRPhBQB/Pregunta6-Intermedio.png', 'si', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta7', 'https://i.postimg.cc/MTwwgSjB/Pregunta7-Intermedio.png', '4', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta8', 'https://i.postimg.cc/NFfcfkDf/Pregunta8-Intermedio.png', '3', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta9', 'https://i.postimg.cc/3rnhfzry/Pregunta9-Intermedio.png', '5040', 'Intermedio');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta10', 'https://i.postimg.cc/sx6RBGHP/Pregunta10-Intermedio.png', 'vida', 'Intermedio');
+
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta1', 'https://i.postimg.cc/pLB3yF9m/Pregunta1-Avanzado.png', '2', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta2', 'https://i.postimg.cc/GhGWwNdJ/Pregunta2-Avanzado.png', '64', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta3', 'https://i.postimg.cc/NjtWg40h/Pregunta3-Avanzado.png', 'Juan 25', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta4', 'https://i.postimg.cc/cC8c3VXD/Pregunta4-Avanzado.png', '23', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta5', 'https://i.postimg.cc/66DkPwJp/Pregunta5-Avanzado.png', '55', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta6', 'https://i.postimg.cc/7b8nnmRq/Pregunta6-Avanzado.png', '6', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta7', 'https://i.postimg.cc/2jbpTytt/Pregunta7-Avanzado.png', '19', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta8', 'https://i.postimg.cc/bvNWnwb3/Pregunta8-Avanzado.png', '10', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta9', 'https://i.postimg.cc/DzjDP3PF/Pregunta9-Avanzado.png', 'no', 'Avanzado');
+INSERT INTO repositorios(titulo,url,respuesta,nivel) VALUES ('Pregunta10', 'https://i.postimg.cc/TwKskNtb/Pregunta10-Avanzado.png', '32', 'Avanzado');
--- frontend-pyquiz/src/components/PruebaBasica.jsx
+++ frontend-pyquiz/src/components/PruebaBasica.jsx
@@ -11,7 +11,8 @@ const PruebaBasica = () =>{
     const [indicePregunta, setIndicePregunta] = useState(0);
     const [finalizado, setFinalizado] = useState(false);
     const [tiempo, setTiempo] = useState(0);
-    
+    const [numeroPregunta, setNumeroPregunta] = useState(1);
+
     useEffect(() => {
         const obtenerPreguntas = async () => {
           try {
@@ -75,22 +76,23 @@ const PruebaBasica = () =>{
     const handleSiguientePregunta = () => {
         if (indicePregunta < 3) {
           setIndicePregunta((prevIndicePregunta) => prevIndicePregunta + 1);
+          setNumeroPregunta((prevNumeroPregunta) => prevNumeroPregunta + 1);
         } else {
           setFinalizado(true);
         }
     };
 
     const renderizarPreguntaActual = () => {
-        const preguntaActual = preguntas[indicePregunta];
+        //const preguntaActual = preguntas[indicePregunta];
         const urlActual = imagenUrl[indicePregunta];
         const respuestaUsuarioActual = respuestasUsuario[indicePregunta];
     
         return (
           <div>
-            <h1 style={{ marginTop: '-80px', textAlign: 'center'}}>¿Qué retorna el código?</h1>
-            <img src={urlActual} alt="Imagen de la pregunta" style={{ width: '600px', height: '400px'}}/>
-            <h1 style={{ textAlign: 'center'}}>Ingrese su respuesta</h1>
-            <p>{preguntaActual}</p>
+            <div className="progress-bar">Pregunta {numeroPregunta}/4</div>
+            <h1 style={{ marginTop: '-100px', textAlign: 'center'}}>¿Qué retorna el código?</h1>
+            <img src={urlActual} alt="Imagen de la pregunta" style={{ maxWidth: '600px', maxHeight: '400px', width: '100%', height: 'auto', marginTop: '20px'}}/>
+            <h1 style={{ marginTop: '10px', textAlign: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>Ingrese su respuesta:</h1>
             <input
               type="text"
               value={respuestaUsuarioActual || ''}
@@ -98,7 +100,8 @@ const PruebaBasica = () =>{
               style={{
                 width: '100%',
                 padding: '5px',
-                textAlign: 'center'
+                textAlign: 'center',
+                marginTop: '10px'
               }}
             />
             <button onClick={handleSiguientePregunta}>
@@ -107,24 +110,6 @@ const PruebaBasica = () =>{
           </div>
         );
     };
-
-    /*
-    const renderizarResultado = () => {
-        const respuestasCorrectas = respuestas.reduce((count, respuesta, index) => {
-          if (respuesta === respuestasUsuario[index]) {
-            return count + 1;
-          }
-          return count;
-        }, 0);
-    
-        return (
-          <div>
-            <h1>Resultado</h1>
-            <p>Respuestas correctas: {respuestasCorrectas}</p>
-            <p>Tiempo transcurrido: {tiempo} segundos</p>
-          </div>
-        );
-    };*/
 
     const renderizarResultado = () => {
       
@@ -137,49 +122,89 @@ const PruebaBasica = () =>{
 
       const puntajeTotal = respuestasCorrectas / 4;
 
-      const preguntasCorrectas = preguntas.reduce((result, pregunta, index) => {
-        if (respuestas[index] === respuestasUsuario[index]) {
-          result.push(index + 1); // Agregar el número de pregunta (index + 1) al arreglo
-        }
-        return result;
-      }, []);
-    
-      const preguntasIncorrectas = preguntas.reduce((result, pregunta, index) => {
-        if (respuestas[index] !== respuestasUsuario[index]) {
-          result.push(index + 1); // Agregar el número de pregunta (index + 1) al arreglo
-        }
-        return result;
-      }, []);
+      const puntajePregunta1 = respuestas[0] === respuestasUsuario[0] ? 7 : 1;
+      const puntajePregunta2 = respuestas[1] === respuestasUsuario[1] ? 7 : 1;
+      const puntajePregunta3 = respuestas[2] === respuestasUsuario[2] ? 7 : 1;
+      const puntajePregunta4 = respuestas[3] === respuestasUsuario[3] ? 7 : 1;
+
+      let mensaje = "";
+      if (puntajeTotal === 7) {
+          mensaje = "FELICITACIONES";
+      } else if (puntajeTotal >= 4) {
+          mensaje = "CASI";
+      } else {
+          mensaje = "DEFICIENTE";
+      }
+
+      let descripcion = "";
+      if (puntajeTotal === 7) {
+          descripcion = "Lograste superar el nivel Básico, estás listo para subir el nivel";
+      } else if (puntajeTotal >= 4) {
+          descripcion = "Estás a punto de superar este desafío, vuelve a intentarlo";
+      } else {
+          descripcion = "Vuelve a estudiar los fundamentos de Python y supera este reto";
+      }
 
       return (
-        <div class="results-container">
-          <h1 class="result-title">Resultado</h1>
-          <div class="result-card">
-            <p class="result-label">Nota obtenida:</p>
-            <p class="result-value" id="correct-answers">{puntajeTotal}</p>
+        <div class="card-container">
+        <div class="card-container-left">
+          <h3>Resultado</h3>
+
+          <div class="card-container-left-circle">
+            <div class="circle-div">
+              <h1>{puntajeTotal}</h1>
+              <p>de 7</p>
+            </div>
           </div>
-          <div class="result-card">
-            <p class="result-label">Tiempo transcurrido:</p>
-            <p class="result-value" id="elapsed-time">{tiempo} segundos</p>
-          </div>
-          <div class="result-card">
-          <h2 class="result-label">Preguntas respondidas correctamente:</h2>
-            <ul>
-              {preguntasCorrectas.map((numeroPregunta) => (
-                <p key={numeroPregunta} class="result-value">Pregunta {numeroPregunta}</p>
-              ))}
-            </ul>
-          </div>
-          <div class="result-card">
-            <h2 class="result-label">Preguntas respondidas incorrectamente:</h2>
-            <ul>
-              {preguntasIncorrectas.map((numeroPregunta) => (
-                <p key={numeroPregunta} class="result-value">Pregunta {numeroPregunta}</p>
-              ))}
-            </ul>
+
+          <div class="card-container-left-text">
+            <h2>{mensaje}</h2>
+            <p class="card-text">
+            {descripcion}
+            </p>
+            <p> Tiempo: {tiempo} segundos</p>
           </div>
         </div>
-      );
+
+        <div class="card-container-right">
+          <h2 style={{ textAlign: 'center'}}>Progreso</h2>
+          <div class="card-right-group">
+            <div class="group reaction">
+              <div class="group-icon reaction-icon">
+                <img src="./assets/images/icon-reaction.svg" alt="" />
+                <p>Pregunta 1</p>
+              </div>
+              <p class="span-b"><b>{puntajePregunta1}</b> / 7</p>
+            </div>
+
+            <div class="group memory">
+              <div class="group-icon memory-icon">
+                <img src="./assets/images/icon-memory.svg" alt="" />
+                <p>Pregunta 2</p>
+              </div>
+              <p class="span-b"><b>{puntajePregunta2}</b> / 7</p>
+            </div>
+
+            <div class="group verbal">
+              <div class="group-icon verbal-icon">
+                <img src="./assets/images/icon-verbal.svg" alt="" />
+                <p>Pregunta 3</p>
+              </div>
+              <p class="span-b"><b>{puntajePregunta3}</b> / 7</p>
+            </div>
+            <div class="group visual">
+              <div class="group-icon visual-icon">
+                <img src="./assets/images/icon-visual.svg" alt="" />
+                <p>Pregunta 4</p>
+              </div>
+              <p class="span-b"><b>{puntajePregunta4}</b> / 7</p>
+            </div>
+          </div>
+
+          <a href="/pruebas"><button> Salir</button></a>
+        </div>
+      </div>
+      )
     };
     
     return (
